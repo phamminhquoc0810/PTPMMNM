0b1bd9a8f9f37504fa506ddb75fdb80d83394ecd
Minh Quoc <quocmp0827@gmail.com>
2024-04-19 08:14:06 +0700

    them sql

---
+++ web_project.sql
@@ -0,0 +1,66 @@
+CREATE TABLE IF NOT EXISTS `order_items` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_image` varchar(255) NOT NULL,
+  `product_price` int(11) NOT NULL,
+  `product_quantity` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`item_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(108) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_password` varchar(100) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `UX_Constraint` (`user_email`)
+);
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_cost` decimal(6,2) NOT NULL,
+  `order_status` varchar(100) NOT NULL DEFAULT 'on_hold',
+  `user_id` int(11) NOT NULL,
+  `user_phone` int(11) NOT NULL,
+  `user_city` varchar(255) NOT NULL,
+  `user_address` varchar(255) NOT NULL,
+  `order_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`order_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(100) NOT NULL,
+  `product_category` varchar(108) NOT NULL,
+  `product_description` varchar(255) NOT NULL,
+  `product_image` varchar(255) NOT NULL,
+  `product_image2` varchar(255),
+  `product_image3` varchar(255),
+  `product_image4` varchar(255),
+  `product_price` decimal(6,2) NOT NULL,
+  `product_special_offer` int(2),
+  `product_color` varchar(108),
+  PRIMARY KEY (`product_id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `payments` (
+	`payment_id` int(11)  NOT NULL AUTO_INCREMENT,
+	`order_id` int(11) NOT NULL,
+	`user_id` int (11) NOT NULL,
+	`transaction_id` varchar(250) NOT NULL,
+	`payment_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`payment_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `admins` (
+	`admin_id` int(11) NOT NULL AUTO_INCREMENT,
+	`admin_name` varchar(250) NOT NULL,
+	`admin_email` text NOT NULL,
+	`admin_password` text NOT NULL,
+	PRIMARY KEY (`admin_id`)
+)
